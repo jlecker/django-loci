f2f4b4f29cb21e3bacba1b37531c3becd170b282
James Lecker <jlecker@gmail.com>
2011-11-09 15:25:28 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+VERSION == __import__('mediaman').__version__
+
+
+setup(
+    name='django-loci',
+    version=VERSION,
+    author='Midwest Communications',
+    description='Place Management and Geolocation App',
+    url='https://github.com/MidwestCommunications/django-loci',
+    packages=['loci']
+)
